74640a97c35d1e0659fa0704bc845f156b2ea6b8
Aryan Verma <vermaaryan50@gmail.com>
2020-05-06 17:15:55 +0530

    phonebook step4 and step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,42 @@
 import React, { useState } from 'react'
 
+const Filter = (props) => <div> filter shown with <input value={props.filterValue} onChange={props.handleNewFilter} /> </div>
+
+const PersonForm = (props) => {
+  return (
+    <div>
+      <form>
+        <div>name: <input value={props.newName} onChange={props.handleNameChange} /></div>
+        <div>number: <input value={props.newNumber} onChange={props.handleNumberChange} /></div>
+        <div><button type="submit" onClick={props.addNumber}>add</button></div>
+      </form>
+    </div>
+  )
+}
+
+const Persons = (props) => <div>{props.personsToShow.map(person => <div key={person.name}>{person.name} {person.number}</div>)}</div>
+
 const App = () => {
-  const [ persons, setPersons ] = useState([]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber] = useState('')
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterValue, setFilterValue] = useState('')
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filterValue.toLowerCase()))
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+  const handleNewFilter = (event) => setFilterValue(event.target.value)
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   const addNumber = (event) => {
     event.preventDefault()
-    console.log(newName)
     const contactObject = {
       name: newName,
       number: newNumber
     }
-    if(persons.findIndex( person => person.name === newName) === -1) {
+    if (persons.findIndex(person => person.name === newName) === -1) {
       setPersons(persons.concat(contactObject))
     } else {
       window.alert(`${newName} is already added to phonebook`)
@@ -32,19 +48,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit" onClick={addNumber}>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>{persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}</div>
+      <Filter filterValue={filterValue} handleNewFilter={handleNewFilter}/>
+      <h3>Add a new</h3>
+      <PersonForm newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} addNumber={addNumber}/>
+      <h3>Numbers</h3>
+      <Persons personsToShow={personsToShow}/>
     </div>
   )
 }
